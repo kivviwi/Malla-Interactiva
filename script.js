ac2c7d48aac97606ba11b040a538523e9258ee87
kivviwi <rafaela.diaz.u@ug.uchile.cl>
2025-07-13 19:55:01 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,10 @@
-// === script.js ===
-
 const cursos = [
-  // SEMESTRE I
   { nombre: "Bases Moleculares", id: "bases-moleculares", semestre: 1, desbloquea: ["bases-moleculares-celulares", "bases-celulares"] },
   { nombre: "Métodos de cuantificación", id: "metodos-cuantificacion", semestre: 1, desbloquea: ["bioestadistica"] },
   { nombre: "Introducción al manejo de la información", id: "manejo-info", semestre: 1, desbloquea: ["bioestadistica"], requiere: ["metodos-cuantificacion"] },
   { nombre: "Diversidad animal", id: "diversidad-animal", semestre: 1, desbloquea: ["ecologia"] },
   { nombre: "Orientación a la medicina veterinaria", id: "orientacion-medvet", semestre: 1 },
 
-  // SEMESTRE II
   { nombre: "Bases Moleculares y celulares", id: "bases-moleculares-celulares", semestre: 2, desbloquea: ["fisiologia-1", "bases-inmunologicas", "agentes-patogenos"], requiere: ["bases-moleculares"] },
   { nombre: "Bases celulares", id: "bases-celulares", semestre: 2, desbloquea: ["fisiologia-1", "agentes-patogenos"], requiere: ["bases-moleculares"] },
   { nombre: "Bioestadística", id: "bioestadistica", semestre: 2, desbloquea: ["epidemiologia", "maig"], requiere: ["metodos-cuantificacion", "manejo-info"] },
@@ -17,7 +13,6 @@ const cursos = [
   { nombre: "Estructura macroscópica del organismo animal", id: "estructura-macroscopica", semestre: 2, desbloquea: ["fisiologia-2", "maat"] },
   { nombre: "Práctica general básica", id: "practica-general", semestre: 2, desbloquea: ["practica-clinica", "practica-campo"] },
 
-  // SEMESTRE III
   { nombre: "Fisiología I", id: "fisiologia-1", semestre: 3, desbloquea: ["fisiologia-2", "farmacologia"] },
   { nombre: "Epidemiología general", id: "epidemiologia", semestre: 3, desbloquea: ["enfermedades-infecciosas"] },
   { nombre: "Estructura macroscópica del organismo animal", id: "estructura-macroscopica", semestre: 3 },
@@ -27,7 +22,6 @@ const cursos = [
   { nombre: "Práctica clínica básica", id: "practica-clinica", semestre: 3 },
   { nombre: "Práctica de campo básica", id: "practica-campo", semestre: 3 },
 
-  // SEMESTRE IV
   { nombre: "Bases inmunológicas", id: "bases-inmunologicas", semestre: 4, desbloquea: ["patologia-1", "tecnicas-diagnosticas"] },
   { nombre: "Fisiología II", id: "fisiologia-2", semestre: 4, desbloquea: ["patologia-1", "nutricion"] },
   { nombre: "Farmacología general", id: "farmacologia", semestre: 4 },
@@ -38,21 +32,18 @@ const cursos = [
   { nombre: "Módulo interno básico", id: "modulo-basico", semestre: 4 },
   { nombre: "MAAT", id: "maat", semestre: 4 },
 
-  // SEMESTRE V
   { nombre: "Enfermedades infecciosas y parasitarias", id: "enfermedades-infecciosas", semestre: 5, desbloquea: ["salud-publica", "patologia-explotaciones"] },
   { nombre: "Patología I", id: "patologia-1", semestre: 5, desbloquea: ["patologia-2", "exploracion-clinica"] },
   { nombre: "Nutrición", id: "nutricion", semestre: 5, desbloquea: ["alimentacion"] },
   { nombre: "Bases de técnicas diagnósticas", id: "tecnicas-diagnosticas", semestre: 5, desbloquea: ["exploracion-clinica"] },
   { nombre: "MAIG", id: "maig", semestre: 5, desbloquea: ["genetica"] },
 
-  // SEMESTRE VI
   { nombre: "Genética básica", id: "genetica", semestre: 6, desbloquea: ["biotecnologia", "reproduccion"] },
   { nombre: "Patología II", id: "patologia-2", semestre: 6, desbloquea: ["patologia-3", "patologia-diagnostica", "biotecnologia", "reproduccion", "medicina-1"] },
   { nombre: "Alimentación", id: "alimentacion", semestre: 6, desbloquea: ["productivos-1", "productivos-2"] },
   { nombre: "Métodos de exploración clínica", id: "exploracion-clinica", semestre: 6, desbloquea: ["medicina-1"] },
   { nombre: "MAAT", id: "maat", semestre: 6 },
 
-  // SEMESTRE VII
   { nombre: "Patología III", id: "patologia-3", semestre: 7, desbloquea: ["patologia-explotaciones"] },
   { nombre: "Patología diagnóstica", id: "patologia-diagnostica", semestre: 7, desbloquea: ["medicina-2", "anestesia-cirugia"] },
   { nombre: "Reproducción", id: "reproduccion", semestre: 7, desbloquea: ["productivos-1", "productivos-2", "obstetricia"] },
@@ -61,17 +52,14 @@ const cursos = [
   { nombre: "Salud pública veterinaria", id: "salud-publica", semestre: 7, desbloquea: ["inocuidad", "gestion-ambiental"] },
   { nombre: "MACA", id: "maca", semestre: 7 },
 
-  // SEMESTRE VIII
   { nombre: "Gestión ambiental", id: "gestion-ambiental", semestre: 8, desbloquea: ["impacto"] },
   { nombre: "Inocuidad de los alimentos", id: "inocuidad", semestre: 8, desbloquea: ["aseguramiento", "mabl"] },
   { nombre: "Manejos productivos I", id: "productivos-1", semestre: 8 },
   { nombre: "Obstetricia y ginecología", id: "obstetricia", semestre: 8 },
-  { nombre: "Medicina nvl I", id: "medicina-1", semestre: 8 },
   { nombre: "Práctica pre-profesional", id: "pre-profesional", semestre: 8 },
   { nombre: "MAPLAN", id: "maplan", semestre: 8 },
   { nombre: "Módulo int. Ciclo pre-profesional", id: "modulo-preprof", semestre: 8 },
 
-  // SEMESTRE IX
   { nombre: "Impacto ambiental", id: "impacto", semestre: 9 },
   { nombre: "Aseguramiento y calidad de alimentos", id: "aseguramiento", semestre: 9 },
   { nombre: "Manejos productivos II", id: "productivos-2", semestre: 9 },
@@ -80,7 +68,6 @@ const cursos = [
   { nombre: "Patología en explotaciones", id: "patologia-explotaciones", semestre: 9 },
   { nombre: "MABL", id: "mabl", semestre: 9 },
 
-  // SEMESTRE X
   { nombre: "Internado medicina individual", id: "internado-individual", semestre: 10 },
   { nombre: "Internado producción animal", id: "internado-produccion", semestre: 10 },
   { nombre: "Internado medicina preventiva", id: "internado-preventiva", semestre: 10 },
